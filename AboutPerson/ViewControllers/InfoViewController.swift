4b44f2bf88964df6ddb7a3ada3789723d1ff6c23
ISkvor <starlingy608@gmail.com>
2022-01-19 04:09:30 +0300

    setup navigation

--- AboutPerson/ViewControllers/InfoViewController.swift
+++ AboutPerson/ViewControllers/InfoViewController.swift
@@ -9,12 +9,22 @@ import UIKit
 
 class InfoViewController: UIViewController {
     
+    @IBOutlet var InfoLabel: UILabel!
+    
     var user: User!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = user.person.fullName
         
-        
+        InfoLabel.text = """
+        Fraction: \(user.person.fraction)
+        Profession: \(user.person.profession)
+        Hobby: \(user.person.hobby)
+        """
     }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let picturesVC = segue.destination as? PicturesViewController else { return }
+        picturesVC.user = user
+    }
 }
--- AboutPerson/ViewControllers/PicturesViewController.swift
+++ AboutPerson/ViewControllers/PicturesViewController.swift
@@ -8,9 +8,13 @@
 import UIKit
 
 class PicturesViewController: UIViewController {
+    
+    @IBOutlet var pictureImageView: UIImageView!
+    
+    var user: User!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        pictureImageView.image = UIImage(named: user.person.image)
     }
-    
 }
